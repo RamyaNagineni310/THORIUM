7b25a6927986133ffeb47047f52efad17e2054f1
RamyaNagineni310 <nagineniramya310@gmail.com>
2023-11-30 19:40:12 +0530

    added footer section

--- app/page.tsx
+++ app/page.tsx
@@ -27,6 +27,31 @@ export default function Home() {
         </p>
       </div>
 
+      <div className="images-section">
+        <div className="person-three-image" />
+        <div className="dot-shape" />
+      </div>
+      <div className="map-section">
+        <h2>
+          Need to offer <i>nationwide</i>
+          <div>access to health care</div>
+          providers?
+        </h2>
+        <div className="flex flex-row gap-10">
+          <div className="md:w-[50%] flex flex-col">
+            <p className="details flex-1">
+              MagnaCare has an extensive PPO network that includes all 50 states
+              and Puerto Rico, with top-rated doctors and hospitals, as well as
+              inpatient and outpatient services such as rehab centers, urgent
+              care centers and ambulatory surgery centers.
+            </p>
+            <span>CONTACT US</span>
+          </div>
+          <div className="md:w-[50%]">
+            <div className="map-image" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
